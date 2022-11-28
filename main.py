3b8ed0f76c21456cf8149d108ef81136627fca39
bogdankozlovsky <bogdan.k@datarails.com>
2022-11-28 13:45:13 +0100

    fix moon weight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import locals
+import PygameUtils as pu
 
 from constants import my_font, screen, earth_weight, moon_weight, earth_diameter, moon_diameter, clock
 from utils import SolarObject, Rocket
@@ -10,11 +11,11 @@ moon = SolarObject("moon", 1 + 0j, 500 + 110j, moon_weight, moon_diameter, (0, 0
 earth = SolarObject("earth", 0 + 0j, 500 + 500j, earth_weight, earth_diameter, (0, 255, 0), my_font, (0, 0, 0), (850, 20))
 rocket = Rocket("rocket", 0 + 0j, 10 + 10j, 1000, 5, (255, 0, 0), my_font, (0, 0, 0), (850, 40))
 key_stat = {locals.K_a: False, locals.K_s: False, locals.K_d: False, locals.K_w: False}
+checkb = pu.checkbox((255, 0, 0), 740, 10, 10, 10, text="tick available", size=20)
 
 running = True
-clock_is_on = True
 while running:
-    if clock_is_on:
+    if checkb.check:
         dt = clock.tick(30) / 1000  # sec
     else:
         dt = None
@@ -41,6 +42,11 @@ while running:
                 key_stat[locals.K_d] = False
             if event.key == locals.K_a:
                 key_stat[locals.K_a] = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if checkb.isOver(pos):
+                checkb.convert()
+
     rocket.control(key_stat)
     moon.change_speed(earth, dt)
     rocket.change_speed(earth, dt)
@@ -49,6 +55,7 @@ while running:
     rocket.change_position(dt)
 
     screen.fill((255, 255, 255))
+    checkb.draw(screen)
 
     moon.draw(screen)
     earth.draw(screen)
